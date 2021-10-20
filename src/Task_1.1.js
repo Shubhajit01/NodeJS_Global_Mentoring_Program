34b82e0380aedee2affa2fa50d02443152b168a4
Shubhajit <shubhajit_chatterjee@epam.com>
2021-10-20 17:25:54 +0530

    Created Shell

--- src/Task_1.1.js
+++
@@ -1,7 +0,0 @@
-const reverseString = (str) => [...str].reverse().join('');
-
-process.stdin.on('data', (data) => {
-	const input = data.toString().trim();
-	const output = reverseString(input);
-	console.log(output);
-});
